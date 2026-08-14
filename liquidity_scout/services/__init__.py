4d40e9d8e60a23ad4d36183a0477bb429c1d516f
Sungjinwoo <254147247+bhaygood29053-pixel@users.noreply.github.com>
2026-08-14 13:05:19 -0400

    Export market presentation services

--- liquidity_scout/services/__init__.py
+++ liquidity_scout/services/__init__.py
@@ -6,12 +6,24 @@ from .market_context import (
     price_movement_label,
     volume_activity_label,
 )
+from .market_presentation import (
+    FIELD_ORDER,
+    format_field_line,
+    full_snapshot_lines,
+    requested_asset_fields,
+    wants_token_address,
+)
 from .market_report import build_market_report
 
 __all__ = [
+    "FIELD_ORDER",
     "build_market_report",
     "build_verified_market_context",
+    "format_field_line",
+    "full_snapshot_lines",
     "liquidity_depth_label",
     "price_movement_label",
+    "requested_asset_fields",
     "volume_activity_label",
+    "wants_token_address",
 ]
